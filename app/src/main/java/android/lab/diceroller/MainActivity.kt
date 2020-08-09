a662244b594cc81b05a30de6827f058ed8dedd93
Lucas Sampaio <lukaz.sampaio@gmail.com>
2020-08-09 16:09:25 -0300

    Iniciando variável de forma atrasada
    Quando o "findViewById()" é guardado em uma val dentro de um método e esse método é executado a cada ação (clique) uma nova referência dessa variável é criada e isso pode causar uma lentidão (caso o app seja bem mais robusto).
    
    Há uma forma de tornar isso melhor que é declarar a variável em escopo de classe com valor null e apenas atribuir o "findViewById()" a essa variável. Porém, sempre que for necessário usar essa variável será preciso checar se ela está nula.
    
    O Kotlin tem um recurso chamado "lazy initialization" que é quando criamos uma variável com "lateinit var myVar: Type". Aqui acaba a necessidade de checar se a variável é nula.

--- app/src/main/java/android/lab/diceroller/MainActivity.kt
+++ app/src/main/java/android/lab/diceroller/MainActivity.kt
@@ -8,6 +8,8 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -29,7 +32,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(diceVal)
     }
 }
